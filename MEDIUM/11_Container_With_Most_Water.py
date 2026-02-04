ebc9f04c380a3cca8f3993b3830f76490859ec05
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-05 00:08:52 +0545

    Container code improved but still time limit exceedes

--- MEDIUM/11_Container_With_Most_Water.py
+++ MEDIUM/11_Container_With_Most_Water.py
@@ -1,4 +1,4 @@
-height = [1, 8, 6, 2, 5, 4, 8, 3, 7, 9, 6, 7]
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 max_area = 1
 height2 = [1, 1]
 
@@ -9,17 +9,13 @@ class Solution:
 
     def maxArea(self, height: List[int]) -> int:
         max_area = 1
-        for index1, value1 in enumerate(height):
-            for index2, value2 in enumerate(height):
-                new_area = self.calculate_area(value1, value2, index2 - index1)
+        for i in range(len(height)):
+            for j in range(i, len(height)):
+                new_area = min(height[i], height[j]) * (j - i)
                 if new_area > max_area:
                     max_area = new_area
-        print(max_area)
-
-    def calculate_area(self, number1, number2, distance):
-        lesser_num = min(number1, number2)
-        return lesser_num * distance
+        return max_area
 
 
 solution = Solution()
-solution.maxArea(height)
+print(solution.maxArea(height))
