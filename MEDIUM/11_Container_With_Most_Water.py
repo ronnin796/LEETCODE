06f855d50acaf24e17633dcac4342960f6736ba4
Nischal Chaudhary <nischal.chaudhary796@gmail.com>
2026-02-07 23:12:28 +0545

    Finally Did the container problem . Learned about two pointers

--- MEDIUM/11_Container_With_Most_Water.py
+++ MEDIUM/11_Container_With_Most_Water.py
@@ -9,7 +9,7 @@ class Solution:
 
     def maxArea(self, height: List[int]) -> int:
         max_area = 1
-        for i in range(len(height)):
+        for i in range(len(height) // 2):
             for j in range(i, len(height)):
                 new_area = min(height[i], height[j]) * (j - i)
                 if new_area > max_area:
